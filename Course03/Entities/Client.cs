d0a64501473aa1d951ee192d048d092d48460bf8
Douglas Borges <douglasborges19@gmail.com>
2023-08-15 19:00:51 -0300

    GetHashCode and Equals

---
+++ Course03/Entities/Client.cs
@@ -0,0 +1,26 @@
+﻿namespace Course03.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Client))
+            {
+                return false;
+            }
+
+            Client other = obj as Client;
+
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ Course03/Program.cs
@@ -0,0 +1,32 @@
+﻿using Course03.Entities;
+using System;
+
+namespace Course03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client()
+            {
+                Name = "Maria",
+                Email = "maria@gmail.com"
+            };
+
+            Client b = new Client()
+            {
+                Name = "Alex",
+                Email = "alex@gmail.com"
+            };
+
+            //Compara o conteúdo
+            Console.WriteLine(a.Equals(b));
+
+            //Compara a referência de memória
+            Console.WriteLine(a == b);
+
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
